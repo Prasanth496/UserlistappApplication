9a6ff92fdfb44b224a1c85e703687e9f177a28cb
Prasanth496 <saiprashanthreddy96@gmail.com>
2020-04-14 15:02:17 +0530

    [prasanth]add. added json file

--- app/src/main/java/com/example/model/UserDataManagerImpl.kt
+++ app/src/main/java/com/example/model/UserDataManagerImpl.kt
@@ -1,11 +1,39 @@
 package com.example.model
 
-class UserDataManagerImpl {
+class UserDataManagerImpl() : UserDataManager {
 
     private val users = arrayListOf<User>()
 
-    fun insertUser(user : User) : Boolean
+    override fun insertUser(user: User): Boolean {
+        doesUserExist(user.email)?.let {
+            return false
+        }
+        return users.add(user)
+    }
+
+    override fun updateUser(user: User): Boolean {
+        val user: User? = doesUserExist(user.email)
+            val it: User? = null
+            val index: Int = users.indexOf(user)
+            users.remove(it)
+        if (user != null) {
+            users.add(index, user)
+        }
+            return true
+        }
+
+    private fun doesUserExist(email : String) : User? = try{
+        users.first{ it.email == email}
+    }catch(exception : NoSuchElementException){
+        null
+    }
+
+    override fun readUsers(): List<User>{
+      return users
+    }
+
+    override fun deleteUsers()
     {
-        return true
+        users.clear()
     }
 }
--- app/src/test/java/com/example/userlist/model/UserDataManagerTest.kt
+++ app/src/test/java/com/example/userlist/model/UserDataManagerTest.kt
@@ -10,9 +10,44 @@ class UserDataManagerTest {
     // function test for create user
     @Test
     fun ShouldBeableToInsertOneUser() {
-        val userDatamanager: UserDataManagerImpl = UserDataManagerImpl()
-        val user = User("Pavan", mobileNumber = "9543216423")
-        Assert.assertTrue(userDatamanager.insertUser(user))
+        val userDataManager: UserDataManager = UserDataManagerImpl()
+        val user = User("Pavan", mobileNumber = "9543216423",email = "pavan123@gmail.com")
+        val userListSizeBeforeAdd : Int = userDataManager.readUsers().size
+        userDataManager.insertUser(user)
+        Assert.assertEquals(userListSizeBeforeAdd + 1, userDataManager.readUsers().size)
 
     }
+    @Test
+    fun ShouldBeableToInsertTwoUsers() {
+        val userDataManager: UserDataManager= UserDataManagerImpl()
+        val userOne = User("Pavan", mobileNumber = "9543216423",email = "pavan123@gmail.com")
+        val userTwo = User("Ravi", mobileNumber = "9645432532",email = "ravi234@gmail.com")
+        val userListSizeBeforeAdd = userDataManager.readUsers().size
+        userDataManager.insertUser(userOne)
+        userDataManager.insertUser(userTwo)
+        Assert.assertEquals(userListSizeBeforeAdd + 2, userDataManager.readUsers().size)
+    }
+    @Test
+    fun ShouldBeableToInsertUsersWithSameEmail() {
+        val userDataManager: UserDataManager= UserDataManagerImpl()
+        val userOne = User("Pavan", mobileNumber = "9543216423",email = "pavan123@gmail.com")
+        val userTwo = User("Ravi", mobileNumber = "9645432532",email = "pavan123@gmail.com")
+        val userListSizeBeforeAdd = userDataManager.readUsers().size
+        userDataManager.insertUser(userOne)
+        Assert.assertTrue(userDataManager.insertUser(userOne))
+        Assert.assertFalse(userDataManager.insertUser(userTwo))
+    }
+
+    @Test
+    fun ShouldBeableToInsertUsersWithDifferentEmail() {
+        val userDataManager: UserDataManager = UserDataManagerImpl()
+        val userOne = User("Pavan", mobileNumber = "9543216423", email = "pavan123@gmail.com")
+        val userTwo = User("Ravi", mobileNumber = "9645432532", email = "ravi432@gmail.com")
+        val userListSizeBeforeAdd = userDataManager.readUsers().size
+        userDataManager.insertUser(userOne)
+        Assert.assertTrue(userDataManager.insertUser(userOne))
+        Assert.assertFalse(userDataManager.insertUser(userTwo))
+    }
+
+
 }
