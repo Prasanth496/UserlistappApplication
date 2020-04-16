2b99b2754637fb9446fde143b706808511461945
Prasanth496 <saiprashanthreddy96@gmail.com>
2020-04-16 14:16:16 +0530

    [prasanth]add. added test for should be able to add two user to UI

--- app/src/test/java/com/example/userlist/model/UserDataPresenterTest.kt
+++ app/src/test/java/com/example/userlist/model/UserDataPresenterTest.kt
@@ -14,8 +14,20 @@ class UserDataPresenterTest {
     @Test
     fun ShouldBeAbleToAddUserToUI() {
         val userDataPresenter: UserDataPresenter = UserDataPresenterImpl()
-        val user = User("Pavan", mobileNumber = "9543216423",email = "pavan123@gmail.com")
+        val user = User("Pavan", mobileNumber = "9543216423", email = "pavan123@gmail.com")
         userDataPresenter.initAddUserUI(user)
         Assert.assertTrue(userDataPresenter.initAddUserUI(user))
+    }
+
+    // test for able to add two users to UI
+    @Test
+    fun ShouldBeAbleToAddTwoUsersToUI() {
+        val userDataPresenter: UserDataPresenter = UserDataPresenterImpl()
+        val userOne = User("Pavan", mobileNumber = "9543216423", email = "pavan123@gmail.com")
+        val userTwo = User("Ravi", mobileNumber = "9645432532",email = "ravi234@gmail.com")
+        userDataPresenter.initAddUserUI(userOne)
+        userDataPresenter.initAddUserUI(userTwo)
+        Assert.assertTrue(userDataPresenter.initAddUserUI(userOne))
+        Assert.assertTrue(userDataPresenter.initAddUserUI(userTwo))
     }
 }
